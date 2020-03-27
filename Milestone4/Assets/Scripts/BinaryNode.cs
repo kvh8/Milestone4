ea01b444540fe6d330ef1ff6a939183b9938a25a
kvh8 <54556865+kvh8@users.noreply.github.com>
2020-03-27 00:09:09 -0700

    added unity code

---
+++ Milestone4/Assets/Scripts/BinaryNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryNode : MonoBehaviour
+{
+	private GameObject prefab;
+	private string value;
+
+	BinaryNode left = null;
+	BinaryNode right = null;
+	BinaryNode(string v = "")
+	{
+		value = v;
+	}
+
+
+	public BinaryNode NextChoice()
+	{
+
+	}
+
+
+
+}
