78f0c58296d02c90c13c6d3b754779f387173e91
kvh8 <54556865+kvh8@users.noreply.github.com>
2020-03-27 17:12:09 -0700

    added code and the start to added my list to a binary tree

--- Milestone4/Assets/Scripts/BinaryNode.cs
+++ Milestone4/Assets/Scripts/BinaryNode.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class BinaryNode : MonoBehaviour
 {
-	private GameObject prefab;
-	private string value;
+	private GameObject value;
+    public GameObject Value { get { return this.value;} }
 
-	BinaryNode left = null;
-	BinaryNode right = null;
-	BinaryNode(string v = "")
+    BinaryNode left = new BinaryNode();
+	BinaryNode right = new BinaryNode();
+	public BinaryNode(GameObject v = null)
 	{
 		value = v;
 	}
 
 
-	public BinaryNode NextChoice()
+
+    public void Insert(List<GameObject> textPrefabs, int index, BinaryNode searchtree)
 	{
+        searchtree.value = value;
+        searchtree.Insert(textPrefabs, (2 * index) + 1, searchtree.left);
+        searchtree.Insert(textPrefabs, (2 * index) + 2, searchtree.right);
+        index++; 
 
-	}
+        //foreach(GameObject item in tree)
+        //{
+        //     if(searchtree.value == null)
+        //     {
+        //         searchtree.value = item;
+        //     }
+        //     else
+        //     {
+        //         searchtree.left.Insert(tree);
+        //         searchtree.right.Insert(tree);
+        //     }
+        //}
 
-
+    }
 
 }
--- Milestone4/Assets/Scripts/GameManager.cs
+++ Milestone4/Assets/Scripts/GameManager.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] textPrefabs;
+    [SerializeField]
+    private Button option1, option2;
+
+    private BinaryNode adventure;
+    private List<GameObject> story;
+
+
+    private int index = 0;
+    private bool isPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        resetGame();
+     
+        adventure.Insert(story, index, adventure);
+
+        while(adventure.Value != null)
+        {
+
+        }
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +38,19 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    private void resetGame()
+    {
+        isPlaying = false;
+        option1.GetComponentInChildren<Text>().text = "Option 1";
+        option2.GetComponentInChildren<Text>().text = "Option 2";
+        option1.gameObject.SetActive(true);
+        option2.gameObject.SetActive(true);
+
+    }
+
+
+
+
+
 }
