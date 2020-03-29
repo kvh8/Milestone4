3ee12dea46ba4cdb1c266f775ff4c223782747a7
kvh8 <54556865+kvh8@users.noreply.github.com>
2020-03-29 08:53:13 -0700

    code updates for the insert into a binary tree as well as sifting through to the next node in the tree

--- Milestone4/Assets/Scripts/BinaryNode.cs
+++ Milestone4/Assets/Scripts/BinaryNode.cs
@@ -6,24 +6,44 @@ using UnityEngine.UI;
 
 public class BinaryNode : MonoBehaviour
 {
-	private GameObject value;
-    public GameObject Value { get { return this.value;} }
+	public GameObject value;
 
-    BinaryNode left = new BinaryNode();
-	BinaryNode right = new BinaryNode();
+
+    public BinaryNode left = null;
+	public BinaryNode right = null;
 	public BinaryNode(GameObject v = null)
 	{
 		value = v;
 	}
 
+    /*void insert(BinaryNode story, GameObject[] prefabs)
+    {
+        int index = 0;
+        while (isPlaying)
+        {
+            story.value = prefabs[index];
+            story.left.Insert(prefabs, (2 * index) + 1);
+            story.right.Insert(prefabs, (2 * index) + 2);
+            index++;
+        }
+
+    }*/
+
+    public void Insert(GameObject[] prefabs, int index)
+    {
+        BinaryNode adventure = new BinaryNode();
+        adventure.value = prefabs[index];
+        adventure.left.Insert(prefabs, (2 * index) + 1);
+        adventure.right.Insert(prefabs, (2 * index) + 2);
+    }
 
 
-    public void Insert(List<GameObject> textPrefabs, int index, BinaryNode searchtree)
-	{
-        searchtree.value = value;
-        searchtree.Insert(textPrefabs, (2 * index) + 1, searchtree.left);
-        searchtree.Insert(textPrefabs, (2 * index) + 2, searchtree.right);
-        index++; 
+    //public void Insert(GameObject[] textPrefabs, int index, BinaryNode searchtree)
+	//{
+   //     searchtree.value = textPrefabs[index];
+   //     searchtree.Insert(textPrefabs, (2 * index) + 1, searchtree.left);
+  //      searchtree.Insert(textPrefabs, (2 * index) + 2, searchtree.right);
+        
 
         //foreach(GameObject item in tree)
         //{
@@ -38,6 +58,5 @@ public class BinaryNode : MonoBehaviour
         //     }
         //}
 
-    }
-
+   // }
 }
--- Milestone4/Assets/Scripts/GameManager.cs
+++ Milestone4/Assets/Scripts/GameManager.cs
@@ -9,43 +9,97 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject[] textPrefabs;
     [SerializeField]
+    private GameObject textPosition;
+    [SerializeField]
     private Button option1, option2;
 
-    private BinaryNode adventure;
-    private List<GameObject> story;
-
-
-    private int index = 0;
-    private bool isPlaying;
+    private bool op1 = false;
+    private bool op2 = false;
+    private bool isPlaying = false;
 
     // Start is called before the first frame update
     void Start()
     {
         resetGame();
-     
-        adventure.Insert(story, index, adventure);
 
-        while(adventure.Value != null)
+
+        BinaryNode adventure = new BinaryNode(textPrefabs[0]);
+        adventure.left = new BinaryNode(textPrefabs[1]);
+        adventure.right = new BinaryNode(textPrefabs[2]);
+        adventure.left.left = new BinaryNode(textPrefabs[3]);
+        adventure.left.right = new BinaryNode(textPrefabs[4]);
+        adventure.right.left = new BinaryNode(textPrefabs[5]);
+        adventure.right.right = new BinaryNode(textPrefabs[6]);
+        adventure.left.left.left = new BinaryNode(textPrefabs[7]);
+        adventure.left.left.right = new BinaryNode(textPrefabs[8]);
+        adventure.left.right.left = new BinaryNode(textPrefabs[9]);
+        adventure.left.right.right = new BinaryNode(textPrefabs[10]);
+        adventure.right.left.left = new BinaryNode(textPrefabs[11]);
+        adventure.right.left.right = new BinaryNode(textPrefabs[12]);
+        adventure.right.right.left = new BinaryNode(textPrefabs[13]);
+        adventure.right.right.right = new BinaryNode(textPrefabs[14]);
+        adventure.left.right.left.left = new BinaryNode(textPrefabs[15]);
+        adventure.left.right.left.right = new BinaryNode(textPrefabs[16]);
+        adventure.right.right.left.left = new BinaryNode(textPrefabs[17]);
+        adventure.right.right.left.right = new BinaryNode(textPrefabs[18]);
+        adventure.left.right.left.right.left = new BinaryNode(textPrefabs[19]);
+        adventure.left.right.left.right.right = new BinaryNode(textPrefabs[20]);
+        adventure.left.right.left.right.left.left = new BinaryNode(textPrefabs[21]);
+        adventure.left.right.left.right.left.right = new BinaryNode(textPrefabs[22]);
+        adventure.left.right.left.right.left.right.left = new BinaryNode(textPrefabs[23]);
+        adventure.left.right.left.right.left.right.right = new BinaryNode(textPrefabs[24]);
+
+
+
+        nextChoice(adventure);
+    }
+
+
+
+
+
+   void nextChoice(BinaryNode jackpot)
+    {
+        while (isPlaying == true)
         {
+            Instantiate(jackpot.value, textPosition.transform);
 
+            option1.onClick.AddListener(delegate
+            {
+                op1 = true;
+            });
+
+            option2.onClick.AddListener(delegate
+            {
+                op2 = true;
+            });
+
+            if (op1 == true)
+            {
+                jackpot = jackpot.left;
+            }
+            else if (op2 == true)
+            {
+                jackpot = jackpot.right;
+            }
         }
 
-
     }
+
 
     // Update is called once per frame
-    void Update()
+   /* void Update()
     {
-        
+
     }
+    */
 
     private void resetGame()
     {
-        isPlaying = false;
-        option1.GetComponentInChildren<Text>().text = "Option 1";
-        option2.GetComponentInChildren<Text>().text = "Option 2";
+        //Instantiate(textPrefabs[0], textPosition.transform);
         option1.gameObject.SetActive(true);
         option2.gameObject.SetActive(true);
+        isPlaying = true;
 
     }
 
